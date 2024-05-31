b2847198a8a41db75ea11b4451d098b0ce068323
yayner2002 <yayner2002@gmail.com>
2024-05-31 15:57:06 +0300

    boiler plate generated with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <div>
+        <h1> Welcome to Practical Products</h1>
+        <p>This is a website that allows us to find list of Products</p>
+      </div>
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p>Practical Products. All Rights Reserved. 2024.</p>
+    </div>
+  );
+}
+
+export default Footer;
