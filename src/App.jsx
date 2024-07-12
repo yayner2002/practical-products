5909696a8fd7eb0307119a523a76ccf70b089b01
yayner2002 <yayner2002@gmail.com>
2024-07-13 00:32:09 +0300

    added useEffect boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./screens/Home";
-import About from "./screens/About";
-import Products from "./screens/AddProductsPage.jsx";
-import Login from "./screens/Login";
-import { useState } from "react";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [resourceType, setResoureType] = useState("posts");
+  const [resourceData, setResoureData] = useState([]);
+
   return (
     <>
-      <Router>
-        <NavBar />
-        <main
-          style={{
-            width: "80%",
-            margin: "auto",
-          }}
-        >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/add-products" element={<Products />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
+      <div>
+        <button onClick={() => setResoureType("posts")}>POSTS</button>
+        <button onClick={() => setResoureType("users")}>USERS</button>
+        <button onClick={() => setResoureType("comments")}>Comments</button>
+      </div>
+      <h3>{resourceType}</h3>
+      {resourceData.map((item) => (
+        <h1 key={item.id}>{JSON.stringify(item)}</h1>
+      ))}
     </>
   );
-}
+};
+
 export default App;
